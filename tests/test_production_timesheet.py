cd292ba93ba916755ed581770d6fbe1862671329
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:33:49 +0100

    Cleanup tests

--- tests/test_production_timesheet.py
+++ tests/test_production_timesheet.py
@@ -2,37 +2,23 @@
 # This file is part production_timesheet module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class ProductionTimesheetTestCase(unittest.TestCase):
-    '''
-    Test Production Timesheet module.
-    '''
+    'Test Production Timesheet module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('production_timesheet')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('production_timesheet')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
